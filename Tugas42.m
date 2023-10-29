b36208c60b9d82472281742265bc29bb7264d3cf
Fabian04A <148173771+Fabian04A@users.noreply.github.com>
2023-10-29 15:02:04 +0700

    Add files via upload

---
+++ Tugas42.m
@@ -0,0 +1,31 @@
+%M Fabian Andriano
+%112022033
+%soal 4.2
+
+clc
+clear
+
+%Matrik identitas dimensi 5 x 5
+%Rumus A = eye(5,5)
+
+A = eye(5,5)
+
+%Matrik dengan dimensi 3 x 3 dengan elemen semuanya bernilai nol
+%Rumus B = zeros(3,3)
+
+B =  zeros(3,3)
+
+%Matrik dengan dimensi 4 x 4 dengan elemen semuanya bernilai satu
+%Rumus C = ones(4,4)
+
+C = ones(4,4)
+
+%Matrik dengan dimensi 3 x 4 dimana elemen terditribusi antara 0 dan 1 (default n = m)
+%Rumus D = rand(3,4)
+
+D = rand(3,4)
+
+%Matrik dengan dimensi 3 x 4 dimana elemen terditribusi normal
+%Rumus E = randn(3,4) 
+
+E = randn(3,4)
